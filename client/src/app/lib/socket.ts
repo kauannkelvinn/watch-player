99ccdc94cb25fb813ad02c10190053d5cdeb4993
kauan <kauankelvin7777@gmail.com>
2026-03-29 17:15:59 -0300

    added render url for vercel

--- client/src/app/lib/socket.ts
+++ client/src/app/lib/socket.ts
@@ -1,7 +1,11 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:3001';
+// Quando fizermos o deploy do server no Render, ele vai te dar uma URL (ex: https://rave-server.onrender.com)
+// Substitua 'SUA_URL_DO_RENDER_AQUI' por ela após o deploy.
+const URL = process.env.NODE_ENV === 'production' 
+  ? 'https://watch-party-server-fvl1.onrender.com' 
+  : 'http://localhost:3001';
 
-export const socket = io(SOCKET_URL, {
-  autoConnect: false, // Só conecta quando a gente mandar
+export const socket = io(URL, {
+  autoConnect: false,
 });
