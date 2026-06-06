9a2bc08036bb007f15913d64f0f59dd1bd699471
kauan <kauankelvin7777@gmail.com>
2026-06-06 18:06:29 -0300

    chore: initialize react native (expo) workspace

---
+++ apps/mobile/metro.config.js
@@ -0,0 +1,18 @@
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '../..');
+
+const config = getDefaultConfig(projectRoot);
+
+// Diz para o Metro "assistir" a pasta principal do monorepo
+config.watchFolders = [workspaceRoot];
+
+// Garante que o Metro procure dependências em ambos os lugares
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
+
+module.exports = config;
